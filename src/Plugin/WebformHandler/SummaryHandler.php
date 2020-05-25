e4d6c61ef7582ed238b202f34fde5929ce138948
Philippe von Bergen <ph.vonbvergen@iqual.ch>
2020-05-25 19:51:32 +0200

    Allow column exclusion, only send fallback if recipient mail is set

--- src/Plugin/WebformHandler/SummaryHandler.php
+++ src/Plugin/WebformHandler/SummaryHandler.php
@@ -25,10 +25,10 @@ class SummaryHandler extends WebformHandlerBase {
    * {@inheritdoc}
    */
   public function getSummary() {
-      // return [
-      //   '#settings' => ['settings' => [$this->t('Recipient email address') => $this->configuration['recipient_mail']]],
-      //   '#theme' => 'webform_handler_settings_summary',
-      // ] + parent::getSummary();
+    // Return [
+    //   '#settings' => ['settings' => [$this->t('Recipient email address') => $this->configuration['recipient_mail']]],
+    //   '#theme' => 'webform_handler_settings_summary',
+    // ] + parent::getSummary();
     return [
       'message' => [
         '#markup' => $this->configuration['recipient_mail'],
@@ -42,13 +42,43 @@ class SummaryHandler extends WebformHandlerBase {
   public function defaultConfiguration() {
     return [
       'recipient_mail' => '',
+      'excluded_elements' => [],
+      'metadata' => FALSE,
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setConfiguration(array $configuration) {
+    parent::setConfiguration($configuration);
+
+    // Make sure 'default' is converted to '_default'.
+    // @see https://www.drupal.org/project/webform/issues/2980470
+    // @see webform_update_8131()
+    // @todo Webform 8.x-6.x: Remove the below code.
+    $default_configuration = $this->defaultConfiguration();
+    foreach ($this->configuration as $key => $value) {
+      if ($value === 'default'
+        && isset($default_configuration[$key])
+        && $default_configuration[$key] === static::DEFAULT_VALUE) {
+        $this->configuration[$key] = static::DEFAULT_VALUE;
+      }
+    }
+
+    return $this;
   }
 
   /**
    * {@inheritDoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['general'] = [
+      '#type' => 'details',
+      '#title' => $this->t('General settings'),
+      '#description' => $this->t(''),
+      '#open' => TRUE,
+    ];
     $form['general']['recipient_mail'] = [
       '#type' => 'email',
       '#title' => $this->t('Recipient email address'),
@@ -57,6 +87,29 @@ class SummaryHandler extends WebformHandlerBase {
       '#required' => TRUE,
       '#maxlength' => 500,
     ];
+    if ($_SERVER['REMOTE_ADDR'] == '83.150.28.13') {
+      // Elements.
+      $form['elements'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Included email values/markup'),
+        '#description' => $this->t('The selected elements will be included in the [webform_submission:values] token. Individual values may still be printed if explicitly specified as a [webform_submission:values:?] in the email body template.'),
+        '#open' => $this->configuration['excluded_elements'] ? TRUE : FALSE,
+      ];
+      $form['elements']['metadata'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Include metadata'),
+        '#description' => $this->t('If checked, metadata fields are included.'),
+        '#return_value' => TRUE,
+        '#default_value' => $this->configuration['metadata'],
+      ];
+      $form['elements']['excluded_elements'] = [
+        '#type' => 'webform_excluded_elements',
+        '#exclude_markup' => FALSE,
+        '#webform_id' => $this->webform->id(),
+        '#default_value' => $this->configuration['excluded_elements'],
+      ];
+    }
+
     $this->elementTokenValidate($form);
     return $this->setSettingsParents($form);
   }
@@ -78,7 +131,21 @@ class SummaryHandler extends WebformHandlerBase {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
-    $this->applyFormStateToConfiguration($form_state);
+
+    $values = $form_state->getValues();
+
+    foreach ($this->configuration as $name => $value) {
+      if (isset($values[$name])) {
+        // Convert options array to safe config array to prevent errors.
+        // @see https://www.drupal.org/node/2297311
+        if (preg_match('/_options$/', $name)) {
+          $this->configuration[$name] = WebformOptionsHelper::encodeConfig($values[$name]);
+        }
+        else {
+          $this->configuration[$name] = $values[$name];
+        }
+      }
+    }
   }
 
   /**
--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -54,15 +54,39 @@ class Mailer {
    */
   protected $useFallback = TRUE;
 
+  protected $excludedColumns = [
+    'uuid' => 'uuid',
+    'token' => 'token',
+    'webform_id' => 'webform_id',
+  ];
+
+  protected $metaColumns = [
+    'serial' => 'serial',
+    'sid' => 'sid',
+    'uri' => 'uri',
+    'created' => 'created',
+    'completed' => 'completed',
+    'changed' => 'changed',
+    'in_draft' => 'in_draft',
+    'current_page' => 'current_page',
+    'remote_addr' => 'remote_addr',
+    'langcode' => 'langcode',
+    'entity_type' => 'entity_type',
+    'entity_id' => 'entity_id',
+    'sticky' => 'sticky',
+    'notes' => 'notes',
+    'uid' => 'uid',
+    'locked' => 'locked',
+  ];
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
     return new static(
-          $container->get('plugin.manager.mail'),
-          $container->get('webform_submission.exporter')
-      );
+        $container->get('plugin.manager.mail'),
+        $container->get('webform_submission.exporter')
+    );
   }
 
   /**
@@ -116,13 +140,22 @@ class Mailer {
   }
 
   /**
+   * Set the range start date.
+   *
+   * @param \DateTime $startDate
+   */
+  public function setExcludedColums(array $excludedColumns) {
+    $this->excludedColumns += $excludedColumns;
+  }
+
+  /**
    * Run the mailing.
    */
   public function run() {
     $webforms = Webform::loadMultiple($this->webformIds);
     $mails = $this->collectFiles($webforms);
     $this->sendMails($mails);
-    $this->cleanup($webforms);
+    $this->cleanup($mails);
   }
 
   /**
@@ -133,74 +166,91 @@ class Mailer {
    */
   protected function collectFiles(array $webforms) {
     // Setup options.
-    $defaultOptions = $this->submissionExporter->getDefaultExportOptions();
-    $defaultOptions['delimiter'] = ';';
-    $defaultOptions['range_type'] = 'date';
-    $defaultOptions['range_start'] = $this->rangeStart->format('Y-m-d');
-    $defaultOptions['range_end'] = $this->rangeEnd->format('Y-m-d');
-    $defaultOptions['destination'] = 'webform_submissions_export';
+    $options = $this->submissionExporter->getDefaultExportOptions();
+    $options['delimiter'] = ';';
+    $options['range_type'] = 'date';
+    $options['range_start'] = $this->rangeStart->format('Y-m-d');
+    $options['range_end'] = $this->rangeEnd->format('Y-m-d');
+    $options['excluded_columns'] = $this->excludedColumns;
+    $options['destination'] = 'webform_submissions_export';
     $mails = [];
     $defaultMail = \Drupal::config('webform_summary.settings')->get('webform_submissions_email');
-    if ($this->useFallback) {
-      $defaultMail = \Drupal::config('webform_summary.settings')->get('webform_submissions_email');
+    $useFallback = $this->useFallback;
+    if ($useFallback && !empty($defaultMail)) {
       $mails = [$defaultMail => []];
+    }
+    else {
+      $useFallback = FALSE;
     }
 
     // Loop through webforms, collect submissions, write files and collect mail info.
     foreach ($webforms as $webform) {
-
-      if (!$this->useFallback) {
-        $hasHandler = FALSE;
+      $webformSubmissions = NULL;
+      $this->submissionExporter->setWebform($webform);
+      $this->submissionExporter->setExporter($options);
+      $query = $this->submissionExporter->getQuery();
+      $sids = $query->execute();
+      // Only write and add mail info if there are submissions in range.
+      if (count($sids) > 0) {
+        // If there are summary handlers on the webform, use the email address from the handler.
         $handlers = $webform->getHandlers();
+        $fallback = TRUE;
         foreach ($handlers as $handler) {
           if ($handler->getPluginId() == 'mail_summary_handler' && $handler->getStatus()) {
             $configuration = $handler->getConfiguration();
             if (!empty($configuration['settings']) && !empty($configuration['settings']['recipient_mail'])) {
-              $hasHandler = TRUE;
-            }
-          }
-        }
-        if (!$hasHandler) {
-          continue;
-        }
-      }
-
-      $this->submissionExporter->setWebform($webform);
-      $this->submissionExporter->setExporter($defaultOptions);
-      $this->submissionExporter->writeHeader();
-      $query = $this->submissionExporter->getQuery();
-      $sids = $query->execute();
-
-      $webformSubmissions = WebformSubmission::loadMultiple($sids);
-
-      // Only write and add mail info if there are submissions in range.
-      if (count($webformSubmissions) > 0) {
-        $this->submissionExporter->writeRecords($webformSubmissions);
-        $file = $webform->id() . '.csv';
-
-        // Make sure the file exists.
-        if (file_exists('/tmp/' . $file)) {
-          // If there are summary handlers on the webform, use the email address from the handler.
-          $handlers = $webform->getHandlers();
-          $fallback = TRUE;
-          foreach ($handlers as $handler) {
-            if ($handler->getPluginId() == 'mail_summary_handler' && $handler->getStatus()) {
-              $configuration = $handler->getConfiguration();
-              if (!empty($configuration['settings']) && !empty($configuration['settings']['recipient_mail'])) {
-                $mails[$configuration['settings']['recipient_mail']][] = $file;
-                $fallback = FALSE;
+              $options['excluded_columns'] = $this->getExcludedColumns($webform, $configuration);
+              $email = $configuration['settings']['recipient_mail'];
+              if ($webformSubmissions == NULL) {
+                $webformSubmissions = WebformSubmission::loadMultiple($sids);
               }
+              $entry = $this->writeFile($webform, $webformSubmissions, $options, $configuration['settings']['recipient_mail']);
+              if ($entry != NULL) {
+                $mails[$email][] = $entry;
+              }
+              $options['excluded_columns'] = $this->excludedColumns;
+              $fallback = FALSE;
             }
           }
+        }
 
-          // Fall back to the default email if there is no handler.
-          if ($this->useFallback && $fallback) {
-            $mails[$defaultMail][] = $file;
+        // Fall back to the default email if there is no handler.
+        if ($useFallback && $fallback) {
+          if ($webformSubmissions == NULL) {
+            $webformSubmissions = WebformSubmission::loadMultiple($sids);
+          }
+          $entry = $this->writeFile($webform, $webformSubmissions, $options, $defaultMail);
+          if ($entry != NULL) {
+            $mails[$defaultMail][] = $entry;
           }
         }
       }
     }
     return $mails;
+  }
+
+  /**
+   * Undocumented function.
+   *
+   * @param [type] $webform
+   * @param [type] $webformSubmissions
+   * @param [type] $options
+   * @param [type] $email
+   *
+   * @return void
+   */
+  protected function writeFile($webform, $webformSubmissions, $options, $email) {
+    $file = $webform->id() . '.csv';
+    $this->submissionExporter->setExporter($options);
+    $this->submissionExporter->writeHeader();
+    $this->submissionExporter->writeRecords($webformSubmissions);
+    // Make sure the file exists.
+    if (file_exists('/tmp/' . $file)) {
+      $personalFilename = '/tmp/' . $webform->id() . '_' . $email . '.csv';
+      rename('/tmp/' . $file, $personalFilename);
+      return ['path' => $personalFilename, 'name' => $file];
+    }
+    return NULL;
   }
 
   /**
@@ -212,19 +262,19 @@ class Mailer {
   protected function sendMails($mails) {
     foreach ($mails as $recipient => $files) {
       $params = ['attachments' => [], 'subject' => 'Webform summary'];
-      foreach ($files as $delta => $file) {
-        $fileContent = iconv("UTF-8", "ISO-8859-1//IGNORE", file_get_contents('/tmp/' . $file));
+      foreach ($files as $delta => $fileInfo) {
+        $fileContent = iconv("UTF-8", "ISO-8859-1//IGNORE", file_get_contents($fileInfo['path']));
         if (!empty($fileContent)) {
           $params['attachments'][] = [
             'filecontent' => $fileContent,
-            'filename' => $file,
+            'filename' => $fileInfo['name'],
             'filemime' => 'text/csv',
           ];
         }
       }
 
       if (!empty($params['attachments'])) {
-        $fileList = implode(', ', $files);
+        $fileList = implode(', ', array_column($files, 'name'));
         if ($this->mailManager->mail('webform_summary', 'webform_summary_csv', $recipient, 'en', $params)) {
           \Drupal::logger('webform_summary')->notice('Sent webform summaries to ' . $recipient . '. File list: ' . $fileList);
         }
@@ -244,12 +294,25 @@ class Mailer {
    * @param array $webforms
    *   The webforms for which to clean the files up.
    */
-  protected function cleanup(array $webforms) {
-    foreach ($webforms as $webform) {
-      if (file_exists('/tmp/' . $webform->id() . '.csv')) {
-        unlink('/tmp/' . $webform->id() . '.csv');
+  protected function cleanup(array $mails) {
+    foreach ($mails as $recipient => $files) {
+      foreach ($files as $delta => $fileInfo) {
+        if (file_exists($fileInfo['path'])) {
+          unlink($fileInfo['path']);
+        }
       }
     }
+  }
+
+  /**
+   *
+   */
+  protected function getExcludedColumns($webform, $handlerConfiguration) {
+    $excludedColumns = $this->excludedColumns + $handlerConfiguration['settings']['excluded_elements'];
+    if (!$handlerConfiguration['settings']['metadata']) {
+      $excludedColumns += $this->metaColumns;
+    }
+    return $excludedColumns;
   }
 
 }
