7e453964ed986830a4a6964a1e5e4cab5081168f
Guillaume Pacilly <g.pacilly@iqual.ch>
2026-01-19 20:16:07 +0100

    TG-1300: add log

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -295,6 +295,7 @@ class Mailer {
       }
       $mailerDisabled = $this->configFactory->get('webform_summary.settings')->get('webform_submissions_disable');
       if ($mailerDisabled) {
+        $this->loggerFactory->get('webform_summary')->notice('mailer is disabled. Did not send webform summaries to ' . $recipient . '.');
         return;
       }
       if (!empty($params['attachments'])) {
